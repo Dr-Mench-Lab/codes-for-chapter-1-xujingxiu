b291a08522de8c98a5348ae46a95adbd5b4a5c26
xujingxiu <75660016+xujingxiu@users.noreply.github.com>
2021-07-13 15:50:51 +0800

    Update charpter2.R

--- charpter2.R
+++ charpter2.R
@@ -57,5 +57,102 @@ library(TeachingDemos)
 ci.examp(mean.sim=10, sd=2, n=25
          , reps=100, conf.level=0.95, method="t")
 
+bs.one.samp.dist <- function(dat, N =1e4){
+  n <- length(dat);
+  sam <- matrix(sample(dat,size= N*n, replace=TRUE), ncol=N);
+  sam.mean <- colMeans(sam);
+  old.par <- par(no.readonly = TRUE)
+  par(mfrow=c(2,1), mar=c(3,2,2,1), oma=c(1,1,1,1))
+  hist(dat, freq=FALSE, breaks=6
+       ,main="Plot of data with smoothed density curve")
+  points(density(dat), type="l")
+  rug(dat)
+  
+  hist(sam.mean,freq=FALSE, breaks=25
+       , main="Bootstrap sampling distribution of the mean"
+       , xlab=paste("Data: n=", n
+                    , ", mean=", signif(mean(dat), digits = 5)
+                    , ", se=", signif(sd(dat)/sqrt(n)),digits=5))
+  points(density(sam.mean),type="l")
+  x <- seq(min(sam.mean), max(sam.mean), length=1000)
+  points(x, dnorm(x, mean = mean(dat),sd=sd(dat)/sqrt(n))
+         ,type = "l", lwd=2, col="red")
+  rug(sam.mean)
+  par(old.par)
+}
+x<- rgamma(10, shape = .5, scale = 20)
+bs.one.samp.dist(x)
+
+age <- c(54,42,51,54,49,56,33,58,54,64,49)
+par(mfrow=c(2,1))
+hist(age, freq=FALSE, breaks=6)
+points(density(age), type="1")
+rug(age)
+
+library(vioplot)
+vioplot(age, horizontal=TRUE, col="gray")
+
+qt(1 - 0.05/2, df=length(age)-1)
+?t.test
+t.summary <- t.test(age, mu=50)
+t.summary
+
+summary(age)
+bs.one.samp.dist(age)
+
+
+
+t.dist.pval<-function(t.summary) {
+  par(mfrow=c(1,1))
+  lim.extreme <- max(4,abs(t.summary$statistic) + 0.5)
+  lim.lower <- -lim.extreme;
+  lim.upper <-lim.extreme;
+  x.curve <-seq(lim.lower,lim.upper,length=200)
+  y.curve <-dt(x.curve,df = t.summary$parameter)
+  plot(x.curve, y.curve, type = "n"
+       , ylab =paste("t-dist( df=", signif(t.summary$parameter, 3), ")")
+       , xlab =paste("t-stat=", signif(t.summary$statistic, 5)
+                     , " ，Shaded area is p-value =", signif(t.summary$p.value, 5)))
+  if ((t.summary$alternative == "less" )
+      | (t.summary$alternative == "two.sided ")) {
+    x.pval.l <- seq(lim.lower,-abs(t.summary$statistic),length=200)
+    y.pval.l <-dt(x.pval.l, df = t.summary$parameter)
+    polygon(c(lim.lower,x.pval.l, -abs(t.summary$statistic))
+            , c(0,y.pval.u,0), col="gray" )
+  }
+  if ((t.summary$alternative == "greater")
+      |(t.summary$alternative == "two.sided")) {
+    x.pval.u <- seq(abs(t.summary$statistic), lim.upper, length=200)
+    y.pval.u <- dt(x.pval.u, df = t.summary$parameter)
+    polygon(c(abs(t.summary$statistic), x.pval.u, lim.upper)
+            , c(0, y.pval.u, 0), col="gray")
+  }
+  points(x.curve, y.curve, type = "l", lwd = 2, col = "blue")
+}
+t.dist.pval(t.summary)
+
+names(t.summary)
+t.summary$statistic
+t.summary$parameter
+t.summary$p.vaule
+t.summary$conf.int
+t.summary$estimate
+t.summary$null.value
+t.summary$alternative
+t.summary$method
+t.summary$data.name
+
+
+toco <- c(5.6,2.7,6.2,2.9,1.5,4.0,4.3,3.0,3.6,2.4,6.7,3.8)
+par(mfrow=c(2,1))
+hist(toco, freq = FALSE, breaks = 6)
+points(density(toco), type = "l")
+rug(toco)
+vioplot(toco, horizontal=TRUE, col="gray" )
+stem(toco,scale=2)
+qt(1-0.05/2,df=length(toco)-1)
+t.summary<-t.test(toco,mu=0.54)
+t.summary
+summary(toco)
 
 
